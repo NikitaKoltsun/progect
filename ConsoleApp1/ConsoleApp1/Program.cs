272373a494eacba4e9247c3f2449f6da0dd2a09a
NikitaKoltsun <46969850+NikitaKoltsun@users.noreply.github.com>
2019-01-23 19:30:33 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите рзмер массива а:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int [n];
+
+
+
+            int m = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < a.GetLength(0); i ++)
+            {
+                a[i] = rnd.Next(1,10);
+            }
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                if (i % 2 == 1)
+                    m++;
+                Console.WriteLine("1");
+            }
+
+            int[] b = new int [m];
+          
+           
+            
+
+            for (int i = 0; i <= m; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+            Console.WriteLine();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
